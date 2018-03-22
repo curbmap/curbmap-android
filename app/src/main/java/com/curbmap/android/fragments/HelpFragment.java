341bcb091e9fab15c8ea060424459c34f0118611
Joshua Herman <me@joshuaherman.org>
2018-03-21 20:10:42 -0700

    fix depreciated imports and app compat support on updated files

--- app/src/main/java/com/curbmap/android/fragments/HelpFragment.java
+++ app/src/main/java/com/curbmap/android/fragments/HelpFragment.java
@@ -15,7 +15,6 @@
 package com.curbmap.android.fragments;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
